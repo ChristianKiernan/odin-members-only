efbc8ea8b56f2abe529b634b29b30293350fce4f
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-05-17 12:35:01 -0400

    Initial two views, and connect to router

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const usersController = require('../controllers/usersController');
 const usersRouter = Router();
 
-usersRouter.get('/', (req, res) => res.render('sign-up'));
+usersRouter.get('/', (req, res) => res.render('signUp'));
 usersRouter.post('/', usersController.createUser);
 
 module.exports = usersRouter;

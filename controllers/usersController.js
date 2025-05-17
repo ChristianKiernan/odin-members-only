27cc444fa9b142f58ead86375b685eafabd970e4
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-05-17 16:11:55 -0400

    Implement password hashing with bcryptjs

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,6 @@
 const db = require('../models/userModel');
 const passport = require('passport');
+const bcrypt = require("bcryptjs");
 
 const signUpPageGet = async (req, res, next) => {
 	res.render('signUp');
@@ -11,12 +12,13 @@ const logIn = passport.authenticate('local', {
 });
 
 const createUser = async (req, res, next) => {
+	const hashedPassword = await bcrypt.hash(req.body.password, 10);
 	try {
 		await db.createUser(
 			req.body.firstName,
 			req.body.lastName,
 			req.body.username,
-			req.body.password
+			hashedPassword
 		);
 
 		res.redirect('/');

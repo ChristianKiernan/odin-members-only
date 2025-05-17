c504df2f8c714a8b1af4ce027ca2956d5d14620f
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-05-17 15:22:27 -0400

    Minor bug fixes regarding routes

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,7 +2,10 @@ const { Router } = require('express');
 const usersController = require('../controllers/usersController');
 const usersRouter = Router();
 
-usersRouter.get('/', usersController.signUpPageGet);
-usersRouter.post('/', usersController.createUser);
+usersRouter.get('/', (req, res) => res.render('index'));
+usersRouter.get('/sign-up', usersController.signUpPageGet);
+usersRouter.get('/success', (req, res) => res.render('success'));
+usersRouter.post('/sign-up', usersController.createUser);
+usersRouter.post('/log-in', usersController.logIn);
 
 module.exports = usersRouter;

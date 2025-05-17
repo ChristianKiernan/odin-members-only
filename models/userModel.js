de641e33460a6e73fc94d979a52ab98a1d914724
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-05-17 10:32:40 -0400

    Create userModel table and createUser method

---
+++ models/userModel.js
@@ -0,0 +1,13 @@
+const pool = require('../config/db');
+
+const createUser = async ({ firstName, lastName, username, password }) => {
+	const result = await pool.query(
+		'INSERT INTO membersonly_users(first_name, last_name, username, password) VALUES($1, $2, $3, $4)',
+		[firstName, lastName, username, password]
+	);
+	return result.rows[0];
+};
+
+module.exports = {
+	createUser,
+};

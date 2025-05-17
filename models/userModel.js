abd3eab2841a6376171a854079b10f520571e89e
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-05-17 16:12:21 -0400

    Minor bug fix

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,9 @@
 const pool = require('../config/db');
 
-const createUser = async (firstName, lastName, username, password) => {
+const createUser = async (firstName, lastName, username, hashedPassword) => {
 	const result = await pool.query(
 		'INSERT INTO membersonly_users(first_name, last_name, username, password) VALUES($1, $2, $3, $4)',
-		[firstName, lastName, username, password]
+		[firstName, lastName, username, hashedPassword]
 	);
 	return result.rows[0];
 };

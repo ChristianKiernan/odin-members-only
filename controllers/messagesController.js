ec5b3d08b2125f33f2c217bfc919a7f56ea6b7b6
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-05-17 20:11:20 -0400

    Add a controller for messages. Get all messages for eventual dsiplay on the homepage, and add functionality to create a new message (users only)

---
+++ controllers/messagesController.js
@@ -0,0 +1,43 @@
+const messageModel = require('../models/messageModel');
+const { body, validationResult } = require('express-validator');
+
+const getHomePage = async (req, res, next) => {
+	try {
+		const messages = await messageModel.getAllMessages();
+		res.render('index', { user: req.user, messages });
+	} catch (err) {
+		next(err);
+	}
+};
+
+const createMessage = [
+	body('title').notEmpty().withMessage('Title is required'),
+	body('text').notEmpty().withMessage('Message text is required'),
+
+	async (req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			const messages = await messageModel.getAllMessages();
+			return res.status(400).render('index', {
+				user: req.user,
+				messages,
+				errors: errors.array(),
+			});
+		}
+		try {
+			await messageModel.createMessage({
+				userId: req.user.id,
+				title: req.body.title,
+				text: req.body.text,
+			});
+			res.redirect('/');
+		} catch (err) {
+			next(err);
+		}
+	},
+];
+
+module.exports = {
+	getHomePage,
+	createMessage,
+};
